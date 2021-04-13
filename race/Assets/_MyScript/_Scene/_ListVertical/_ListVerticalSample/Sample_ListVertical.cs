a894cf24a12d38e6406b11a458166be3a0ac9df0
Khoa Pham Dang <80452561+PDK13@users.noreply.github.com>
2021-04-13 19:58:07 +0700

    Updated _MyScript
    Xoá chỗ có Firebase

--- race/Assets/_MyScript/_Scene/_ListVertical/_ListVerticalSample/Sample_ListVertical.cs
+++ race/Assets/_MyScript/_Scene/_ListVertical/_ListVerticalSample/Sample_ListVertical.cs
@@ -1,9 +1,4 @@
-using Firebase.Database;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Sample_ListVertical : MonoBehaviour
 {
